7ff3fc15a91e9a1d9b9326893c78903093cbde82
emmalu0309 <emmalu0309@icloud.com>
2024-10-29 14:53:39 +0800

    登入及註冊分開

--- login.js
+++ login.js
@@ -1,26 +1,6 @@
 // 用戶數據存取
 const users = []; // 簡單的模擬數據
 
-// // 註冊功能
-// document.getElementById('registerBtn').addEventListener('click', function() {
-//     const username = document.getElementById('registerUsername').value;
-//     const password = document.getElementById('registerPassword').value;
-//     const messageDiv = document.getElementById('message');
-
-//     // 簡單的註冊重複檢查
-//     if (users.find(user => user.username === username)) {
-//         messageDiv.textContent = '帳號重複，請選擇其他帳號名稱';
-//         return;
-//     }
-
-//     // 註冊用戶
-//     users.push({ username, password });
-//     messageDiv.textContent = '註冊成功，請登入';
-//     window.location.href = "login.html";
-
-
-
-
 // 登入功能
 document.getElementById('loginBtn').addEventListener('click', function() {
     const username = document.getElementById('loginUsername').value;
